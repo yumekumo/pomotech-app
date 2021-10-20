f64005f489a060cabb100005ca6859ba0961f89d
yumekumo <chihachiha.ryotakun@gmail.com>
2021-10-20 17:44:23 +0000

    [Add] 投稿機能の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,23 +3,38 @@ class TasksController < ApplicationController
     @tasks = current_user.tasks
     @task = Task.new
   end
-  
+
   def create
+    @task = Task.new(task_params)
+    @task.is_finished = false
+    @task.user_id = current_user.id
+    if @task.save
+      redirect_to task_path(@task), notice: "You have created task successfully."
+    else
+      @tasks = current_user.tasks
+      render 'index'
+    end
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def delete
   end
-  
+
   def timer
   end
-  
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :body)
+  end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
   has_many :pomodoros, dependent: :nullify
+  validates :title, presence: true
+  validates :body, length: { maximum: 200 }
 end
