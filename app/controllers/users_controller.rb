dfd2bc65412806ab54370a6f6ac5a419c6724cf9
yumekumo <chihachiha.ryotakun@gmail.com>
2021-10-24 13:51:45 +0000

    [Add] User編集機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+  
   def edit
   end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to tasks_path, notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+  
 end
