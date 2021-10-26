c3c26d04779943f091dda07f1c9e3ef7a0f1691b
yumekumo <chihachiha.ryotakun@gmail.com>
2021-10-26 08:15:05 +0000

    [Update] homes/about -> info に変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
-  resources :users
+  resources :users, only: [:edit, :update]
   resources :tasks, only: [:index, :create, :show, :edit, :update, :destroy] do
     member do
       get 'timer'
